2d390157226f954f7c11cfa6d047f2e260ca981d
mosaab <musab.ehab@gmail.com>
2018-02-08 13:40:16 +0200

    add home.html

--- rate/rating/views.py
+++ rate/rating/views.py
@@ -2,7 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
 from .models import Doctor,University,Faculty
 
+
+class UniversityListView(ListView):
+    model = University
+    template_name = 'home.html'
+    context_object_name = 'univs'
 
 
